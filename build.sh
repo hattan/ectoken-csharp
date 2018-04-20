73c4a066ba7cb3f1690723e80ae950523bbd3090
Hattan Shobokshi <hattan@gmail.com>
2018-04-20 01:44:54 +0000

    added pr check for build.sh, so nuget packges are not pushed on pr

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 dotnet restore
 dotnet build ec_encryptcore
 dotnet test ECToken.Test
-dotnet pack -o dist /p:PackageVersion=1.0.$TRAVIS_BUILD_NUMBER-alpha ecencryptstdlib/ecencryptstdlib.csproj
-dotnet nuget push ecencryptstdlib/dist/ECToken.1.0.$TRAVIS_BUILD_NUMBER-alpha.nupkg --source https://api.nuget.org/v3/index.json --api-key $nugetKey
+if [ $TRAVIS_PULL_REQUEST = "false" ]
+then
+  dotnet pack -o dist /p:PackageVersion=1.0.$TRAVIS_BUILD_NUMBER-alpha ecencryptstdlib/ecencryptstdlib.csproj
+  dotnet nuget push ecencryptstdlib/dist/ECToken.1.0.$TRAVIS_BUILD_NUMBER-alpha.nupkg --source https://api.nuget.org/v3/index.json --api-key $nugetKey
+fi
+
