b4ecd0f4b4becfd4bf6ea600ba957046b449d604
Hattan Shobokshi <hshobokshi@verizondigitalmedia.com>
2018-04-19 18:07:15 -0700

    added build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+dotnet restore
+dotnet build ec_encryptcore
+dotnet test ECToken.Test
+dotnet pack -o dist /p:PackageVersion=1.0.$TRAVIS_BUILD_NUMBER-alpha ecencryptstdlib/ecencryptstdlib.csproj
+dotnet nuget push "ecencryptstdlib/dist/ECToken.1.0.$TRAVIS_BUILD_NUMBER-alpha.nupkg" --source "https://api.nuget.org/v3/index.json" --api-key "$nugetKey"
