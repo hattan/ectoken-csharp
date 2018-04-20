e868e70f14b57583c279de76c2f6573bda51a5ae
hattan <hattan@gmail.com>
2018-04-19 18:58:05 -0700

    added master branch only to travis yml (#4)
    * added master branch only to travis yml
    
    * added master only to appveyor yml
    
    * removed alpha from build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ dotnet build ec_encryptcore
 dotnet test ECToken.Test
 if [ $TRAVIS_PULL_REQUEST = "false" ]
 then
-  dotnet pack -o dist /p:PackageVersion=1.0.$TRAVIS_BUILD_NUMBER-alpha ecencryptstdlib/ecencryptstdlib.csproj
-  dotnet nuget push ecencryptstdlib/dist/ECToken.1.0.$TRAVIS_BUILD_NUMBER-alpha.nupkg --source https://api.nuget.org/v3/index.json --api-key $nugetKey
+  dotnet pack -o dist /p:PackageVersion=1.0.$TRAVIS_BUILD_NUMBER ecencryptstdlib/ecencryptstdlib.csproj
+  dotnet nuget push ecencryptstdlib/dist/ECToken.1.0.$TRAVIS_BUILD_NUMBER.nupkg --source https://api.nuget.org/v3/index.json --api-key $nugetKey
 fi
 
