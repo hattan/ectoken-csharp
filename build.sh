d34043b6194988ddcbe927d2b572d526ebe1e74a
Hattan Shobokshi <hattan@gmail.com>
2018-04-20 01:24:38 +0000

    build.sh changes

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ dotnet restore
 dotnet build ec_encryptcore
 dotnet test ECToken.Test
 dotnet pack -o dist /p:PackageVersion=1.0.$TRAVIS_BUILD_NUMBER-alpha ecencryptstdlib/ecencryptstdlib.csproj
-dotnet nuget push "ecencryptstdlib/dist/ECToken.1.0.$TRAVIS_BUILD_NUMBER-alpha.nupkg" --source "https://api.nuget.org/v3/index.json" --api-key "$nugetKey"
+dotnet nuget push ecencryptstdlib/dist/ECToken.1.0.$TRAVIS_BUILD_NUMBER-alpha.nupkg --source https://api.nuget.org/v3/index.json --api-key $nugetKey
